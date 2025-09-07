3fea972a321184722ddd2fada033fe4a6ab97e46
JRcard <jrm.ricard@gmail.comm>
2025-09-07 11:18:12 -0400

    mise en place de l'audio de la section 3 de la page 1

--- script/script.js
+++ script/script.js
@@ -2,13 +2,13 @@ import { setupAudioPlayer } from "./audio_player.js";
 
 /* AUDIO PLAYERS  */
 /* audio-003-A */
-setupAudioPlayer("https://stream.mux.com/O4h5z00885HEucNNa1rV02wZapcGp01FXXoJd35AHmGX7g/audio.m4a", "play-003-A", "rewind-003-A", "duree-003-A");
+setupAudioPlayer("../audio/Rapport_003-A.mp3", "play-003-A", "rewind-003-A", "duree-003-A");
 
 /* audio-003-B */
-setupAudioPlayer("https://stream.mux.com/O4h5z00885HEucNNa1rV02wZapcGp01FXXoJd35AHmGX7g/audio.m4a", "play-003-B", "rewind-003-B", "duree-003-B");
+setupAudioPlayer("../audio/Rapport_003-B.mp3", "play-003-B", "rewind-003-B", "duree-003-B");
 
 /* audio-003-C */
-setupAudioPlayer("https://stream.mux.com/O4h5z00885HEucNNa1rV02wZapcGp01FXXoJd35AHmGX7g/audio.m4a", "play-003-C", "rewind-003-C", "duree-003-C");
+setupAudioPlayer("../audio/Rapport_003-C.mp3", "play-003-C", "rewind-003-C", "duree-003-C");
 
 /* Animate on scroll */
 AOS.init();
